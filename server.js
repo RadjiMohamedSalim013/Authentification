e727ea0b0291ee985a50b6b978fa1511c480b3a3
Radji Mohamed <radjimohamed013@gmail.com>
2025-06-07 15:40:41 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from "cors";
+import 'dotenv/config'
+import cookie from "cookie-parser";
+import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
+import authRouter from "./routes/authRoutes.js";
+import userRouter from './routes/userRoutes.js';
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB();
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  origin: '*', 
+  credentials: false 
+}));
+
+//app.use('/', require('./routes/authRoutes'))
+
+//API Endpoints
+
+app.get('/', (req, res) => res.send ("API Working"));
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+app.listen(port, () => {
+  console.log(`Server is running on Port: ${port}!!!`)
+})
+
